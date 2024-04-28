e0a51c39948793e6d7858321d22f5bfdcccc6875
possiblyHugo <alainhandzar@gmail.com>
2024-04-27 22:32:45 -0700

    + User input and output + GetCharacter function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,54 @@
+#include <cctype>
+#include <string>
 #include <iostream>
 
+char GetCharacter(char character, int key) {
+	if (!isalpha(character)) {
+		return character;
+	}
+	int combinedDistance = 0;
+
+	if (isupper(character)) {
+		combinedDistance = (int(character) - 65) + key;
+		if (combinedDistance > 25) {
+			return char((combinedDistance) % 26) + 65;
+		}
+		else {
+			return char((combinedDistance) + 65);
+		}
+
+	}
+	else {
+		combinedDistance = (int(character) - 97) + key;
+		if (combinedDistance > 25) {
+			return char((combinedDistance) % 26) + 97;
+		}
+		else {
+			return char((combinedDistance)+97);
+		}
+	}
+}
 
 int main() {
+	std::string phrase;
+	int key = 0;
 
-	std::cout << (0 + 1) % 26;
+	std::cout << "String to encode: ";
+	getline(std::cin, phrase);
+	
+	std::cout << "Key amount: ";
+	std::cin >> key;
+
+	int lowerStart = 97;
+	int lowerEnd = 122;
+
+	int upperStart = 65;
+	int upperEnd = 90;
+
+
+	for (auto character : phrase) {
+		std::cout << GetCharacter(character, key);
+	}
 
 	return 0;
 }
